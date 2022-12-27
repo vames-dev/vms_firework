22e10164a1b9ef55e3b2d5ae2ba392e26134f412
vames-dev <60104107+vames-dev@users.noreply.github.com>
2022-12-27 19:33:44 +0100

    Delete items.sql

--- items.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `items`(`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES
-(0, 'fajerwerki1', '🎆 Fajerwerki', 500, 0, 1),
-(0, 'fajerwerki2', '🎇 Fajerwerki', 500, 0, 1),
-(0, 'fajerwerki3', '🎇 Fontanna', 500, 0, 1);
